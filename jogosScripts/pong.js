1e014c7455d19d3b1a8da263ad9c22104faad36a
Gabriel da Silva Santos <gabrieldasilvasantos@pop-os.localdomain>
2023-11-01 18:14:39 -0300

    pong

--- jogosScripts/pong.js
+++ jogosScripts/pong.js
@@ -1,74 +1,71 @@
-let dimensionar = canva.width/1500*2.45
+let dimensionar = canva.width/290
 let posicionar = canva.width/10
 let teclas = {}
 let bola={
-    x: canva.width/2 - 10/dimensionar,
-    y: canva.height/2 - 10/dimensionar,
-    altura: 10/dimensionar,
-    largura: 10/dimensionar,
+    x: canva.width/2 - 10*dimensionar,
+    y: canva.height/2 - 10*dimensionar,
+    altura: 10*dimensionar,
+    largura: 10*dimensionar,
     dirx: -1,
     dirY: 1,
     mod: 0,
     speed: 1
 }
 let esquerda = {
-    x: 2.5,
-    y: canva.height/2 - 12.5,
-    altura: 25,
-    largura: 7.5,
+    x: 0,
+    y: 12.5,
+    altura: 7.5,
+    largura: canva.width,
     score: 0,
     speed: 3
 }
 let direita = {
-    x: 290,
-    y: canva.height/2 - 12.5,
-    altura: 25,
-    largura: 7.5,
+    x: canva.width/2 - 12.5,
+    y: canva.height - 45*dimensionar,
+    altura: 7.5,
+    largura: 35*dimensionar,
     score: 0,
     speed: 3
 }
 
 function movebloco(){
-    if(key.ArrowUp.pressionado && esquerda.y > 0)
+    if(pincel.ativo)
+        direita.x = posicao.x
+    if(key.ArrowLeft.pressionado && direita.x > 0)
     {
-        esquerda.y -= esquerda.speed
+        direita.x -= direita.speed
+        console.log('opa')
     }
-    if(key.ArrowDown.pressionado && esquerda.y + esquerda.altura < canva.height)
+    if(key.ArrowRight.pressionado && direita.x + direita.largura < canva.width)
     {
-       esquerda.y += esquerda.speed
-    }
-    if(38 in teclas && direita.y > 0){
-        direita.y -= direita.speed
-    }
-    if(40 in teclas && direita.y + direita.altura < canva.height){
-        direita.y += direita.speed
+       direita.x += direita.speed
     }
 }
 function movebola(){
-    if(esquerda.x + esquerda.largura >= bola.x && esquerda.y <= bola.y + bola.altura && esquerda.y + esquerda.altura > bola.y)
+    if(esquerda.y + esquerda.altura >= bola.y && esquerda.y <= bola.y + bola.altura && esquerda.y + esquerda.altura > bola.y)
     {
-        bola.dirx = 1 
-        bola.mod += 0.09
+        bola.dirY = 1 
+        bola.mod += 0.12
     } 
-    if(bola.y + bola.altura >= direita.y && bola.y <= direita.y + direita.altura && bola.x + bola.largura >= direita.x){
-        bola.dirx = -1
-        bola.mod += 0.09
-    }
-    if(bola.y <= 0){
-        bola.dirY = 1
-        bola.mod += 0.09
-    } 
-    if(bola.y + bola.altura >= canva.height){
+    if(bola.y + bola.altura >= direita.y && bola.y <= direita.y && bola.x + bola.largura <= direita.x + direita.largura && bola.x > direita.x){
         bola.dirY = -1
-        bola.mod += 0.09
+        bola.mod += 0.12
     }
+    if(bola.x <= 0){
+        bola.dirx = 1
+        bola.mod += 0.12
+    } 
+    if(bola.x + bola.largura >= canva.width){
+        bola.dirx = -1
+        bola.mod += 0.12
+    }
+
     bola.x += (bola.speed + bola.mod) * bola.dirx
     bola.y += (bola.speed + bola.mod) * bola.dirY
-    if(bola.x < esquerda.x + esquerda.largura - 2.5){
+
+    if(bola.y + bola.altura > direita.y + direita.altura + 20 ){
         newgame("Player 2")
-    } else if(bola.x + bola.largura > direita.x + 7.5){
-        newgame('Player 1')
-    }
+    } 
 }
 function newgame(winner){
     if(winner == "Player 1"){
@@ -76,8 +73,7 @@ function newgame(winner){
     } else{
         direita.score++
     }
-    esquerda.y = canva.height/2 - esquerda.altura/2 
-    direita.y = esquerda.y
+    direita.x = canva.width/2 - 12.5
     bola.y = canva.height/2 - bola.altura/2 
     bola.x = canva.width/2 - bola.largura/2
     bola.mod = 0
