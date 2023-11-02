eab3a8c52260f621a12f28dc60caa2e71069dab5
Gabriel da Silva Santos <gabrieldasilvasantos@pop-os.localdomain>
2023-11-02 00:03:42 -0300

    eba2

--- jogosScripts/labirinto.js
+++ jogosScripts/labirinto.js
@@ -12,7 +12,7 @@ let paredesLabirinto = [
     new Parede({posicao: {x: 79*dimensionar + posicionar, y: 94*dimensionar}, dimensao: {l: 42*dimensionar, a: 11*dimensionar}},src),  
     new Parede({posicao: {x: 23*dimensionar + posicionar, y: 129*dimensionar}, dimensao: {l: 98*dimensionar, a: 11*dimensionar}},src), 
     new Parede({posicao: {x: 21*dimensionar + posicionar, y: 92*dimensionar}, dimensao: {l: 34*dimensionar, a: 11*dimensionar}},src), 
-    new Parede({posicao: {x: 105*dimensionar + posicionar, y: 53*dimensionar}, dimensao: {l: 41*dimensionar, a: 11*dimensionar}},src),  // ,-
+    new Parede({posicao: {x: 103*dimensionar + posicionar, y: 53*dimensionar}, dimensao: {l: 48*dimensionar, a: 11*dimensionar}},src),  // <-
     new Parede({posicao: {x: 46*dimensionar + posicionar, y: 171*dimensionar}, dimensao: {l: 98*dimensionar, a: 11*dimensionar}},src), 
     new Parede({posicao: {x: 46*dimensionar + posicionar, y: 209*dimensionar}, dimensao: {l: 74*dimensionar, a: 11*dimensionar}},src),  
     new Parede({posicao: {x: 110*dimensionar + posicionar, y: 363*dimensionar}, dimensao: {l: 167*dimensionar, a: 11*dimensionar}},src), 
@@ -72,7 +72,7 @@ let paredesLabirinto = [
     new Parede({posicao: {x: 492*dimensionar + posicionar, y: 325*dimensionar}, dimensao: {l: 11*dimensionar, a: 279*dimensionar}},src),
     new Parede({posicao: {x: 77*dimensionar + posicionar, y: 51*dimensionar}, dimensao: {l: 11*dimensionar, a: 54*dimensionar}},src), 
     new Parede({posicao: {x: 110*dimensionar + posicionar, y: 98*dimensionar}, dimensao: {l: 11*dimensionar, a: 40*dimensionar}},src),
-    new Parede({posicao: {x: 140*dimensionar + posicionar, y: 53*dimensionar}, dimensao: {l: 11*dimensionar, a: 154*dimensionar}},src), // <-
+    new Parede({posicao: {x: 140*dimensionar + posicionar, y: 55*dimensionar}, dimensao: {l: 11*dimensionar, a: 154*dimensionar}},src), // <-
     new Parede({posicao: {x: 109*dimensionar + posicionar, y: 209*dimensionar}, dimensao: {l: 11*dimensionar, a: 163*dimensionar}},src),
     new Parede({posicao: {x: 139*dimensionar + posicionar, y: 367*dimensionar}, dimensao: {l: 11*dimensionar, a: 158*dimensionar}},src), 
     new Parede({posicao: {x: 205*dimensionar + posicionar, y: 558*dimensionar}, dimensao: {l: 11*dimensionar, a: 41*dimensionar}},src), 
@@ -122,7 +122,7 @@ let paredesLabirinto = [
     new Parede({posicao: {x: 301*dimensionar + posicionar, y: 175*dimensionar}, dimensao: {l: 11*dimensionar, a: 48*dimensionar}},src),
     new Parede({posicao: {x: 299*dimensionar + posicionar, y: 258*dimensionar}, dimensao: {l: 11*dimensionar, a: 31*dimensionar}},src),
     new Parede({posicao: {x: 96*dimensionar + posicionar, y: 586*dimensionar}, dimensao: {l: 11*dimensionar, a: 14*dimensionar}},src),
-    new Parede({posicao: {x: 122*dimensionar + posicionar, y: 19*dimensionar}, dimensao: {l: 11*dimensionar, a: 18*dimensionar}},src), // <-
+    new Parede({posicao: {x: 113*dimensionar + posicionar, y: 19*dimensionar}, dimensao: {l: 11*dimensionar, a: 18*dimensionar}},src), // <-
     new Parede({posicao: {x: 237*dimensionar + posicionar, y: 40*dimensionar}, dimensao: {l: 11*dimensionar, a: 22*dimensionar}},src),
     new Parede({posicao: {x: 363*dimensionar + posicionar, y: 40*dimensionar}, dimensao: {l: 11*dimensionar, a: 29*dimensionar}},src),
     new Parede({posicao: {x: 301*dimensionar + posicionar, y: 161*dimensionar}, dimensao: {l: 11*dimensionar, a: 24*dimensionar}},src),
